4541e4b516970922f758234a672aaad43c8478ef
Olphen-PC <kesh.andrade@gmail.com>
2023-10-21 00:50:43 +0200

    change Ejercici1,2,3.py

--- Python/Condicionales/Ejercicio1,2,3.py
+++ Python/Condicionales/Ejercicio1,2,3.py
@@ -1,4 +1,4 @@
-'''la f la he colocado para poder llamar las variables denro de las comillas y colocandolas dentro de llaves'''
+#la f la he colocado para poder llamar las variables denro de las comillas y colocandolas dentro de llaves
 a = int(input('Introdueix un número: '))
 b = int(input('Introdueix un altre número: '))
 print(f'El número guardat a la variable a és: {a}')
@@ -9,7 +9,7 @@ print('El resultat de la multiplicació: ',a*b)
 print('El resultat de la divisió: ',a/b)
 print(f'Valor de a: {a}')
 print(f'Valor de b: {b}')
-'''Pero en python se puede hacer a,b = b,a, solo que con la variable c se puede aprender para otros lenguajes'''
+#Pero en python se puede hacer a,b = b,a, solo que con la variable c se puede aprender para otros lenguajes
 c=a
 a=b
 b=c
