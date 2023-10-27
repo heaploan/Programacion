6f65261038e02a891247051ae77d34db0037255b
Hector <hector@mbp-de-hector.lan>
2023-10-27 22:26:15 +0200

    commenting ejercicio3.py

--- Python/Listas/ejercicio3.py
+++ Python/Listas/ejercicio3.py
@@ -1,5 +1,10 @@
+#creamos una lista con 5 valores tipo texto.
 lista=["Fol", "lampara", "galleta", "mouse", "mano"]
+#creamos lista2 vacia para asignarle los valores despues
 lista2=[]
+#usamos extend para agregar todos los valores de lista
 lista2.extend(lista)
+#usamos reverse para poner al reves los valores de tipo texto
 lista2.reverse()
+#imprimimos lista para mostrar el cambio.
 print(lista2)
