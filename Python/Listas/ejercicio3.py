3370bd6423f18065f94079d2a6620d3d2611b710
Olphen-PC <kesh.andrade@gmail.com>
2023-10-28 15:21:55 +0200

    comment

--- Python/Listas/ejercicio3.py
+++ Python/Listas/ejercicio3.py
@@ -4,7 +4,7 @@ lista=["Fol", "lampara", "galleta", "mouse", "mano"]
 lista2=[]
 #usamos extend para agregar todos los valores de lista
 lista2.extend(lista)
-#usamos reverse para poner al reves los valores de tipo texto
+#usamos reverse para ordenar al reves los valores.
 lista2.reverse()
 #imprimimos lista para mostrar el cambio.
 print(lista2)
