6ebf920efab7d4b536e64735fadd2165f8359e7d
Olphen-PC <kesh.andrade@gmail.com>
2023-10-28 18:27:34 +0200

    comentarios ejercicio5.py

--- Python/Listas/ejercicio5.py
+++ Python/Listas/ejercicio5.py
@@ -1,7 +1,14 @@
+#creamos lista vacía
 lista=[]
+#creamos variable num con valor 0 para ponerle valor despues
 num=0
+#mientras que num sea mayor o igual a 0 pedirá introducir numeros.
 while num >= 0:
+    #pedimos valor a num.
     num=int(input())
+    #si num es mayor o igual a 0 se añade el valor a la lista
     if num >=0:
+        #añadimos los valores de num a la lista.
         lista.append(num)
+#imprimimos lista.
 print(lista)
