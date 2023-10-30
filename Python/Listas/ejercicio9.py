88a91403b2acc2f4c0c7fc2c9e7aaa8ceb1ac13e
Olphen-PC <kesh.andrade@gmail.com>
2023-10-30 22:42:38 +0100

    ejercicio9.py comment

--- Python/Listas/ejercicio9.py
+++ Python/Listas/ejercicio9.py
@@ -1,6 +1,8 @@
 nombres=[]
 edades=[]
 nom=""
+#creamos while para que mientras el valor de nom sea diferente a * se haga el bucle
+#si es * deja de hacerse el bucle.
 while nom != "*":
     nom=input("Introduce el nombre del alumno: ")
     if nom != "*":
@@ -10,10 +12,17 @@ while nom != "*":
 maximo=edades[0]
 mayor=nombres[0]
 print("Alumnos mayores de edad: ")
+#generamos este for para mover la posicion en el rango de la longitud de edades.
 for posicio in range(len(edades)):
+    #si edades es mayor que maximo
     if edades[posicio] > maximo:
+        #mayor es igual a la posicion en nombres
         mayor = nombres[posicio]
+        #mayor es igual a la posicion en edades
         maximo = edades[posicio]
+    #si edades es igual o mayor a 18
     if edades[posicio] >= 18:
+        #imprimimos la posicion de los nombres y edades.
         print(nombres[posicio], edades[posicio])
+#imprimimos el alumno mayor de todos y su edad. 
 print(f"El alumno mayor es: {mayor} con {maximo} años de edad.")
