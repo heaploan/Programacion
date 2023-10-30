7c3698371f244164b214edffaf300f238568a3b7
Hector <hector@MacBook-Pro-de-Hector.local>
2023-10-30 18:17:07 +0100

    commenting ejercicio1.py

--- Python/Cadenas/ejercicio1.py
+++ Python/Cadenas/ejercicio1.py
@@ -1,6 +1,8 @@
 nombre=input("Ingrese su nombre completo: ")
+#hace una lista con ambos introducidos.
 o=nombre.split(" ")
 print(nombre.lower())
 print(nombre.upper())
+#si queremos que ponga la primera mayuscula de los dos tanto apellido como nombre, creamos este for.
 for i in o:
     print(i.capitalize(), end=" ")
