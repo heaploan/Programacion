1e1eaf71278bd93a8aac067dfaf73328de4d511b
Hector <hector@MacBook-Pro-de-Hector.local>
2023-11-02 17:47:00 +0100

    correccion ejercicio8

--- Python/Listas/ejercicio8.py
+++ Python/Listas/ejercicio8.py
@@ -4,13 +4,12 @@ lista2=[]
 lista3=[]
 #creamos 3 for, 2 para agregar 5 numeros para lista1 y lista2
 for i in range(5):
+    #pedimos 5 numeros para agregar a lista 1
     num=int(input("Introduce cinco numeros aleatorios para la primer lista: "))
     lista1.append(num)
-for i in range(5):
-        num2=int(input("Introduce cinco numeros aleatorios para la segunda lista: "))
-        lista2.append(num2)
-#este tercer for es el que hará el calculo de lista1 y lista2
-for i in range(5):
+    #pedimos otros 5 numeros para agregar a lista 2
+    num2=int(input("Introduce cinco numeros aleatorios para la segunda lista: "))
+    lista2.append(num2)
     #suma es igual a lista1 con rango i mas lista2 con rango i para sumar los valores de cada mismo lugar en ambas listas.
     suma=lista1[i]+lista2[i]
     #colocamos los valores totales de la suma en lista3
