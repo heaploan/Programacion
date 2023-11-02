3fd3216d94f552fc8aae111c59ed048aa6590584
Olphen-PC <kesh.andrade@gmail.com>
2023-11-02 20:42:40 +0100

    commenting ejercicio8 & ejercicio8

--- Python/Cadenas/ejercicio7.py
+++ Python/Cadenas/ejercicio7.py
@@ -1,11 +1,20 @@
+#creamos la variable z con valor True
 z=True
+#mientras z sea True entramos en bucle
 while z == True:
+    #creamos la variable numero con input
     numero=input("Escribe un numero entero positivo: ")
+    #si el contenido de numero es numerico
     if numero.isnumeric():
+        #el input de numero se convierte en int(input())
         numero=int(numero)
+        #si numero es mayor a 0
         if numero > 0:
+            #si el contenido de numero dividido entre 1 es igual a 0
             if numero % 1 == 0:
+                #la condición se cumple, entonces imprimimos el mensaje
                 print("¡Muy bien, me has hecho caso!")
+                #z deja de ser True y salimos del bucle
                 z=False
             else:
                 print("No has puesto un numero entero.")
--- Python/Cadenas/ejercicio8.py
+++ Python/Cadenas/ejercicio8.py
@@ -1,6 +1,12 @@
+#creamos la variable frase con input
 frase=input("Introduce una frase: ")
+#creamos la variable consonante con "" sin espacio para indicar que es texto el contenido
 con=""
+#creamos la variable for para recorrer los valores de frase
 for i in frase:
+    #si los valores de frase "i" no son aeiou
     if i.lower() not in "aeiou":    
+        #se le suman los valores de frase "i" a con
         con+=i
+#se imprime con y se mostrarían solo las consonantes.
 print(con)
