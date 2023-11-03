b7aef7ca1298b373ab036ebd877b899eb90daa1f
Hector <hector@MacBook-Pro-de-Hector.local>
2023-11-03 14:58:14 +0100

    ohh

--- Python/Cadenas/ejercicio7.py
+++ Python/Cadenas/ejercicio7.py
@@ -1,9 +1,13 @@
 z=True
 while z == True:
     numero=input("Escribe un numero entero positivo: ")
+    #si numero es numerico
     if numero.isnumeric():
+        #el input de la variable numero se convierte en int(input())
         numero=int(numero)
+        #si numero es mayor a 0
         if numero > 0:
+            #si la division de numero 
             if numero % 1 == 0:
                 print("¡Muy bien, me has hecho caso!")
                 z=False
