0bb4e101992e418b649c18efe05fa228eea7a8e8
Hector <hector@MacBook-Pro-de-Hector.local>
2023-11-03 17:08:19 +0100

    oh yeah

--- Python/Cadenas/ejercicio10.py
+++ Python/Cadenas/ejercicio10.py
@@ -8,7 +8,7 @@ while entry != "si":
     entry = entry.lower()  
     #si entry es igual a no, continuamos
     if entry == "no":
-        print("Continuamos.")
+        print("¡Al infinito y mas alla!")
     #si entry es diferente a si, se pone algo al azar, imprime el mensaje de error.
     elif entry != "si":
         print("Por favor, responde 'si' o 'no'.")
