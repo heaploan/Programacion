075fc432508e26561ecf53bc62d7d4e8817547c5
Hector <hector@mbp-de-hector.lan>
2023-11-03 20:46:34 +0100

    parking modified

--- Jo-El/Bucles/Parking.py
+++ Jo-El/Bucles/Parking.py
@@ -1,7 +1,26 @@
 longitud=int(input())
+eleccion=0
+contador=0
+inicio=-1
+longitud_sitio=0
+minimo=9000000
 entrada=input()
-datos=entrada.split()
-inicio=int(datos[0])
-final=int(datos[1])
-while inicio != 0:
+while longitud != 0:
+    contador+=1
+    while longitudEspacio != 0:
+        datos=entrada.split()
+        inicio=int(datos[0])
+        final=int(datos[1])
+        if inicio != 0:
+            longitudEspacio=final-inicio
+            if longitudEspacio>longitud*1.5:
+                if minimo > longitudEspacio:
+                    minimo=longitudEspacio
+                    eleccion=contador
+    entrada=input()
+if minimo == 9000000:
+    print("NO")
+else:
+    print(f"SI {eleccion}")
+        
     
