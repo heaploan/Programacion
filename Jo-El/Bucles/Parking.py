1fc68ca41b735e48f4041706d18a02c3ea25c39a
Olphen-PC <kesh.andrade@gmail.com>
2023-11-04 02:53:15 +0100

    commenting Parking.py

--- Jo-El/Bucles/Parking.py
+++ Jo-El/Bucles/Parking.py
@@ -1,26 +1,54 @@
-longitud=int(input())
-eleccion=0
-contador=0
-inicio=-1
-longitud_sitio=0
-minimo=9000000
-entrada=input()
-while longitud != 0:
-    contador+=1
-    while longitudEspacio != 0:
-        datos=entrada.split()
-        inicio=int(datos[0])
-        final=int(datos[1])
-        if inicio != 0:
-            longitudEspacio=final-inicio
-            if longitudEspacio>longitud*1.5:
-                if minimo > longitudEspacio:
-                    minimo=longitudEspacio
-                    eleccion=contador
-    entrada=input()
-if minimo == 9000000:
-    print("NO")
-else:
-    print(f"SI {eleccion}")
-        
-    
+#creamos aparcar como booleano en True
+aparcar = True
+#mientras aparcar sea True se entra en el bucle
+while aparcar:
+    #creamos la variable para introducir la longitud del coche.
+    longitudCoche = int(input())
+    #creamos una variable para mas tarde introducir datos.
+    mejorPlaza = 0
+    #creamos esta variable como booleano en False para mas tarde cambiarlo.
+    encontradaPlaza = False
+    #si longitudCoche es igual a 0
+    if longitudCoche == 0:
+        #aparcar pasa a ser falso.
+        aparcar = False
+    #mientras no sea falso
+    else:
+        #buscar plaza con booleano en True
+        buscarPlaza = True
+        #creamos contador de plazas
+        contadorPlaza = 0
+        #mientras buscarPlaza sea True
+        while buscarPlaza:
+            #introducimos valores a posiblePlaza
+            posiblePlaza = input()
+            #si posiblePlaza es igual a 0 en texto (porque le input es texto)
+            if posiblePlaza == "0":
+                #buscarPlaza es falso
+                buscarPlaza = False
+            #mientras no sea falso
+            else:
+                #introducimos los valores en split a parking
+                parking = posiblePlaza.split(" ")
+                #la longitudPlaza es igual al final menos el inicio del tamaño del parking (si fuera al revés, podría ser negativo el valor y no queremos eso.)
+                longitudPlaza = int(parking[1])-int(parking[0])
+                #se suma 1 a contadorPlaza
+                contadorPlaza += 1
+                #si la longitudCoche por 50% mas es menor o igual a longitudPlaza
+                if (longitudCoche * 1.5) <= longitudPlaza:
+                    #si mejorPlaza es mayor a LongitudPlaza o encontradaPlaza es igual a falso
+                    if mejorPlaza > longitudPlaza or encontradaPlaza == False:
+                        #encontrada plaza es igual a verdadero
+                        encontradaPlaza = True
+                        #creamos la variable plaza que guarda el valor del contadorPlaza
+                        plaza = contadorPlaza
+                        #mejor plaza coge el valor de longitudPlaza
+                        mejorPlaza = longitudPlaza
+        #si encontradaPlaza es falso (no se ha encontrado)
+        if not encontradaPlaza:
+            #imprimimos NO
+            print("NO")
+        #en caso de haber encontrado plaza (encontradaPlaza es True)
+        else:
+            #imprimimos SI y la variable plaza (el contenido de esta)
+            print(f"SI {plaza}")
