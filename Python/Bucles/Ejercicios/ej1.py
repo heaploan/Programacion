cdef824b4b538f80826ba7044c9c639be8de6105
Apolo <kesh.andrade@gmail.com>
2023-11-19 21:08:07 +0100

    a

--- Python/Bucles/Ejercicios/ej1.py
+++ Python/Bucles/Ejercicios/ej1.py
@@ -1,5 +1,12 @@
-#para que incluya el 0 habrá que poner 21 para que incluya el 10 ya que se empieza a contar desde 0.
-#en este caso el primer numero es 5, el cual indica que se empieza a contar desde el 5, el 21 para que incluya el numero 20
-#el 3 es para saltar de 3 en 3 los numeros.
-for numero in range(5,21,3):
-    print(numero)
+#David Moldovan y Hector Lopez
+#Ejercicio 1
+#pedimos un numero entre el 2 y 20
+num = int(input("Introduce un numero entre el 2 y el 20: "))
+#Si el numero introducido por el usuario esta en el rango hara lo siguiente
+if 1 < num < 21:
+    #Este for mostrara una sucesion de numeros del 1 al numero introducido
+    for i in range(1,num+1):
+        print(i)
+#Si la condicion no se cumple mostrara este mensaje de error
+else:
+    print("El numero que has introducido esta fuera del rango (2-20)")
