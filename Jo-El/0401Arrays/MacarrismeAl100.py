b3d4ee3dc4a8fdb28a9399eebe1e16271852c511
Apolo <kesh.andrade@gmail.com>
2023-11-22 16:32:09 +0100

    MacarrismeAl100.py added

---
+++ Jo-El/0401Arrays/MacarrismeAl100.py
@@ -0,0 +1,12 @@
+# Pedimos al usuario que ingrese la cantidad de casos
+casos = int(input())
+# Pedimos al usuario que ingrese los números separados por espacios y los almacenamos en una lista
+numeros = input().split()
+# Iteramos a través de cada valor en la lista de números
+for valor in numeros:
+    # Convertimos cada valor a punto flotante (float)
+    valorFloat = float(valor)
+    # Calculamos el porcentaje multiplicando el valor por 100
+    porcentaje = valorFloat * 100
+    # Imprimimos el porcentaje con un solo decimal y seguido de un espacio en la misma línea
+    print(f"{porcentaje:.1f}%", end=" ")
