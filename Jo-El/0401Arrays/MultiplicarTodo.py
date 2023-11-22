70ceb635a26550b951ba60e332167a4a300c94a2
Apolo <kesh.andrade@gmail.com>
2023-11-22 14:51:59 +0100

    MultiplicarTodo.py modified and improved

--- Jo-El/0401Arrays/MultiplicarTodo.py
+++ Jo-El/0401Arrays/MultiplicarTodo.py
@@ -1,18 +1,16 @@
-# Solicita la cantidad de casos de prueba
+# Solicita al usuario ingresar la cantidad de casos de prueba
 casos = int(input())
 # Itera a través de cada caso de prueba
 for i in range(casos):
-    # Para cada caso, solicita la cantidad de números en la lista (k)
+    # Solicita al usuario ingresar la cantidad de números en la lista para el caso actual (k)
     k = int(input())
-    # Lee la lista de números como una cadena y la divide en una lista de cadenas
-    numbers = input().split()
-    # Solicita el valor por el cual multiplicar cada número en la lista (p)
-    p = int(input())
+    # Lee la lista de números como una cadena y la divide en una lista de cadenas, utilizando espacios como separadores
+    numbers = input().split(" ")
+    # Solicita al usuario ingresar el valor por el cual multiplicar cada número en la lista (p)
+    p = int(input())  
     # Itera a través de cada número en la lista
     for num in numbers:
-        # Multiplica el número por el valor p y almacena el resultado en 'resultado'
-        resultado = int(num) * p
-        # Imprime el resultado, seguido de un espacio en la misma línea
-        print(resultado, end=" ")
-    # Después de imprimir todos los resultados para un caso, agrega un salto de línea
+        # Multiplica el número (convertido a entero) por el valor p y lo imprime, seguido de un espacio en la misma línea
+        print(int(num) * p, end=" ")
+    # Después de imprimir todos los resultados para un caso, agrega un salto de línea para pasar al siguiente caso
     print()
