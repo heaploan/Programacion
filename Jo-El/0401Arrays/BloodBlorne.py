936405943f10d104f6dcaba45c62d0bb734fcc9a
Apolo <kesh.andrade@gmail.com>
2023-11-22 11:00:33 +0100

    commented bloodborne.py

--- Jo-El/0401Arrays/BloodBlorne.py
+++ Jo-El/0401Arrays/BloodBlorne.py
@@ -7,17 +7,17 @@ for j in range(n):
     # Lee los números como cadenas y crea una lista
     numbers = input().split()
     # Inicializa la variable que indica si hay duplicados
-    found_duplicate = False
+    duplicate = False
     # Inicializa el índice para recorrer el array
     i = 0
     # Itera sobre el array hasta el penúltimo elemento
-    while i < k - 1 and not found_duplicate:
+    while i < k - 1 and not duplicate:
         # Convierte los elementos a enteros y verifica si son iguales
-        found_duplicate = int(numbers[i]) == int(numbers[i + 1])
+        duplicate = int(numbers[i]) == int(numbers[i + 1])
         # Incrementa el índice
         i += 1
     # Imprime "SI" si se encontraron duplicados, "NO" en caso contrario
-    if found_duplicate:
+    if duplicate:
         print("SI")
     else:
         print("NO")
