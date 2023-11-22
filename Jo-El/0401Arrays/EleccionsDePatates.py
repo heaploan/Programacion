d177ab47eb432d5b6c67f1985090f53c2759f3f4
Apolo <kesh.andrade@gmail.com>
2023-11-22 13:53:39 +0100

    Eleccion de patatas comentado

--- Jo-El/0401Arrays/EleccionsDePatates.py
+++ Jo-El/0401Arrays/EleccionsDePatates.py
@@ -1,13 +1,22 @@
+# Se lee el número de casos que se van a evaluar
 casos = int(input())
+# Se itera sobre cada caso
 for i in range(casos):
-    nums = input().split()
-    opcions = int(nums[0])
+    # Se lee una línea que contiene el número de opciones seguido de los valores de esas opciones
+    nums = input().split()   
+    # Se obtiene el número de opciones
+    opciones = int(nums[0])  
+    # Se inicializan variables para la opción ganadora y el índice máximo
     opcionGanadora = 0
-    indiceMax = 0
-    for j in range(1, opcions + 1 ):
-        actual = int(nums[j])
+    indiceMax = 0 
+    # Se itera sobre los valores de las opciones
+    for j in range(1, opciones + 1):
+        # Se obtiene el valor actual
+        actual = int(nums[j])   
+        # Se verifica si el valor actual es mayor que el índice máximo
         if actual > indiceMax:
+            # Si es mayor, se actualiza la opción ganadora y el índice máximo
             opcionGanadora = j
-            indiceMax = actual
+            indiceMax = actual 
+    # Se imprime la opción ganadora para este caso
     print(opcionGanadora)
-    
