025b35f7a625b2b696f5a237393b09cbe394c6ca
Apolo <kesh.andrade@gmail.com>
2023-11-23 13:28:35 +0100

    macarrismeal100.py commented

--- Jo-El/0401Arrays/MacarrismeAl100.py
+++ Jo-El/0401Arrays/MacarrismeAl100.py
@@ -5,8 +5,7 @@ numeros = input().split()
 # Iteramos a través de cada valor en la lista de números
 for valor in numeros:
     # Convertimos cada valor a punto flotante (float)
-    valorFloat = float(valor)
+    valorFloat = float(valor) * 100
     # Calculamos el porcentaje multiplicando el valor por 100
-    porcentaje = valorFloat * 100
     # Imprimimos el porcentaje con un solo decimal y seguido de un espacio en la misma línea
-    print(f"{porcentaje:.1f}%", end=" ")
+    print(f"{valorFloat:.1f}%", end=" ")
