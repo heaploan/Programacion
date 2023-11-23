995e63b7dcf9f27a9317912e66cf17f321d46fcf
Apolo <kesh.andrade@gmail.com>
2023-11-23 23:27:48 +0100

    webcasteller editado y mejorado

--- Jo-El/0403StringsSimples/GraciesWebCasteller.py
+++ Jo-El/0403StringsSimples/GraciesWebCasteller.py
@@ -2,18 +2,17 @@ casos = int(input())
 for i in range(casos):
     palabra1 = input()
     palabra2 = input()
-    lenPalabra1 = len(palabra1)
-    lenPalabra2 = len(palabra2)
-    maxLongitud = lenPalabra1
-    if lenPalabra2 > lenPalabra1:
-        maxLongitud = lenPalabra2
-    palabra1 += " " * (maxLongitud - lenPalabra1)
-    palabra2 += " " * (maxLongitud - lenPalabra2)
-    coincidencias = 0
-    for j in range(maxLongitud):
+    if len(palabra1) > len(palabra2):
+        maxLongitud = len(palabra1)
+        minLongitud = len(palabra2)
+    else:
+        maxLongitud = len(palabra2)
+        minLongitud = len(palabra1)
+    contador = 0
+    for j in range(minLongitud):
         if palabra1[j] == palabra2[j]:
-            coincidencias += 1
-    if coincidencias >= maxLongitud / 2:
+            contador += 1
+    if maxLongitud / 2 <= contador:
         print("GRACIES WEBCASTELLER")
     else:
         print("WEBCASTELLER ESTA TRAVIESO HOY")
