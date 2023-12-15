6678735aa68d36300858069c0e3597b59434256e
heaploan <kesh.andrade@gmail.com>
2023-12-15 16:15:01 +0100

    main.py modificado, def hechos.

--- Python/UF2/Actividad2/main.py
+++ Python/UF2/Actividad2/main.py
@@ -1,49 +1,55 @@
 from user import *
 from dates import *
 dictionary = {}
-
-options = int(input("1. Registrar usuario\n2. Login\n3. Mostrar datos de un usuario\n4. Salir\nIntroduce una opción: "))
-
-while options != 4:
-    if options == 1:
-        user = input("Introduce tu nombre de usuario (debe tener al menos 4 letras): ")
-        if user in dictionary:
-            print("ERROR: Usuario ya registrado")
-        else:
-            key = input("Crear contraseña (debe tener al menos 6 caracteres y al menos una vocal): ")
-            if validarUsuario(user, key): 
-                dictionary[user] = {}
-                dictionary[user]["password"] = key
-                dictionary[user]["age"] = pedirEdad()
-                dictionary[user]["birthdate"] = pedirNacimiento()
-                print("Usuari registrat correctament!")
-            else:
-                print("ERROR: El usuario o contraseña no cumplen con los requisitos indicados.")
-
-    elif options == 2:
-        user = input("Introduce tu nombre de usuario: ")
-        key = input("Introduce tu contraseña: ")
-        if login(dictionary, user, key):
-            print(f"¡Bienvenido, {user}!")
-            print(avui())
-            if aniversari(dictionary[user]["birthdate"]):
-                print("¡Es el mes de tu cumpleaños!")
-            else:
-                print("Este mes no es tu cumpleaños.")
-            print(quantFalta(dictionary[user]["birthdate"]))
-        else:
-            print("ERROR: El usuario o la contraseña son incorrectos.")
-    
-    elif options == 3:
-        user = input("Introduce el usuario que quieres ver: ")
-        if user in dictionary:
-            print(f"Datos de {user}")
-            print("Edad:", dictionary[user]["age"])
-            print("Próximo cumpleaños:", properAniversari(dictionary[user]["birthdate"]))
-        else:
-            print("ERROR: No existe dicho usuario.")
-
+def registrar():
+    user = input("Introduce tu nombre de usuario (debe tener al menos 4 letras): ")
+    if user in dictionary:
+        print("ERROR: Usuario ya registrado")
     else:
-        print("ERROR: ¡La opción no es correcta!")
+        key = input("Crear contraseña (debe tener al menos 6 caracteres y al menos una vocal): ")
+        if validarUsuario(user, key): 
+            dictionary[user] = {}
+            dictionary[user]["password"] = key
+            dictionary[user]["age"] = pedirEdad()
+            dictionary[user]["birthdate"] = pedirNacimiento()
+            print("Usuari registrat correctament!")
+        else:
+            print("ERROR: El usuario o contraseña no cumplen con los requisitos indicados.")
 
+def login():
+    user = input("Introduce tu nombre de usuario: ")
+    key = input("Introduce tu contraseña: ")
+    if login(dictionary, user, key):
+        print(f"¡Bienvenido, {user}!")
+        print(avui())
+        if aniversari(dictionary[user]["birthdate"]):
+            print("¡Es el mes de tu cumpleaños!")
+        else:
+            print("Este mes no es tu cumpleaños.")
+        print(quantFalta(dictionary[user]["birthdate"]))
+    else:
+        print("ERROR: El usuario o la contraseña son incorrectos.")
+
+def mostrarUsuario():
+    user = input("Introduce el usuario que quieres ver: ")
+    if user in dictionary:
+        print(f"Datos de {user}")
+        print("Edad:", dictionary[user]["age"])
+        print("Próximo cumpleaños:", properAniversari(dictionary[user]["birthdate"]))
+    else:
+        print("ERROR: No existe dicho usuario.")
+
+def menu ():
     options = int(input("1. Registrar usuario\n2. Login\n3. Mostrar datos de un usuario\n4. Salir\nIntroduce una opción: "))
+    while options != 4:
+        if options == 1:
+            registrar()
+        elif options == 2:
+            login()
+        elif options == 3:
+            mostrarUsuario()
+        else:
+            print("ERROR: ¡La opción no es correcta!")
+        options = int(input("1. Registrar usuario\n2. Login\n3. Mostrar datos de un usuario\n4. Salir\nIntroduce una opción: "))
+        
+menu()
