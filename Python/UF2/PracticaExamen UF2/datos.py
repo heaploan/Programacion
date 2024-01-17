570b1aa7ccd45746606a05503f3243c181714058
Apolo <kesh.andrade@gmail.com>
2024-01-17 17:34:31 +0100

    agregados archivos de curso python

---
+++ Python/UF2/PracticaExamen UF2/datos.py
@@ -0,0 +1,34 @@
+libros = {}
+prestecs = {}
+def addLlibre():
+    codi = input('Introduce el código: ')
+    if codi in libros:
+        print('ERROR: El código ya existe')
+    else:
+        libros['codi'] = codi
+    titol = input('Introduce el título: ')
+    libros['títol'] = titol
+    autor = input('Introduce el autor: ')
+    libros['autor'] = autor
+    genere = input('Introduce el género: ')
+    libros['genere'] = genere
+    numDePaginas = input('Introduce el número de páginas: ')
+    libros['páginas'] = numDePaginas
+    return libros
+    
+addLlibre()
+print(libros)
+
+def startPrestec():
+    codiDelLlibre = input('Introduce el código del libro: ')
+    if codiDelLlibre in libros['codi']:
+        prestecs['codiLlibre'] = codiDelLlibre
+    else:
+        print('ERROR: El código no existe.')
+    nomAlumne = input('Introduce el nombre del alumno: ')
+    prestecs['Alumne'] = nomAlumne
+    dataPrestec = input('Introduce la fecha de inicio: ')
+    prestecs['Prestecs'] = dataPrestec
+
+startPrestec()
+print(prestecs) 
