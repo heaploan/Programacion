fd2b8eba5c012db75301ad540a354f4974de63c6
Apolo <kesh.andrade@gmail.com>
2024-01-17 16:13:59 +0100

    agregados archivos de curso python

---
+++ Python/Curso Python/loops.py
@@ -0,0 +1,13 @@
+# El loop while es algo que se va a repetir hasta que se cumpla una condición de salida.
+# i = 0
+
+# while i < 5:
+#     i += 1
+#     if i == 3:
+#         continue # si llega a 3, para el while.
+#     print(i)
+
+usuarios = ['Chanchito feliz', 'Felipe', 'Roberto', 'Nicolas']
+
+for usuario in usuarios:
+    print(usuario)
