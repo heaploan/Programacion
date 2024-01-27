9ae6c3ef1860ad2278677b7647cced2635cfddba
Apolo <kesh.andrade@gmail.com>
2024-01-27 09:33:21 +0100

    practica para examen2

--- Python/UF2/Practica Examen 2/main.py
+++ Python/UF2/Practica Examen 2/main.py
@@ -1,32 +1,32 @@
 from interpreter import *
 check = True
 while check:
-    command = input('>').lower().split('-')
-    if command[0] == "addllibre":
+    command = input('>').split('-')
+    if command[0].lower() == "addllibre":
         addLlibre(command)
-    elif command[0] == "startprestec":
+    elif command[0].lower() == "startprestec":
         startPrestec(command)
     #TO DO
-    #elif command[0] == "endprestec":
+    #elif command[0].lower() == "endprestec":
         #endPrestec(command)
-    elif command[0] == "listllibres":
+    elif command[0].lower() == "listllibres":
         listLlibres(command)
     #TO DO
-    #elif command[0] == "listprestecs":
+    #elif command[0].lower() == "listprestecs":
         #listPrestecs(command)
     #TO DO
-    #elif command[0] == "listgenere":
+    #elif command[0].lower() == "listgenere":
         #listGenere(command)
     #TO DO 
-    #elif command[0] == "maxllibre":
+    #elif command[0].lower() == "maxllibre":
         #maxLlibre(command)
     #TO DO 
-    #elif command[0] == "stats":
+    #elif command[0].lower() == "stats":
         #stats(command)
     #TO DO 
-    #elif command[0] == "info":
+    #elif command[0].lower() == "info":
         #info(command)
-    elif command[0] == "quit":
+    elif command[0].lower() == "quit":
         check = False
     else:
         print("ERROR: comando incorrecto")
