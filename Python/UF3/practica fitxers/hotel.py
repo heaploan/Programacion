6a3264ebbb55eac2b7a40f25b920dfab5e2a87a1
heaploan <kesh.andrade@gmail.com>
2024-02-06 20:37:05 +0100

    actividad files modified

---
+++ Python/UF3/practica fitxers/hotel.py
@@ -0,0 +1,18 @@
+# Habitación -> nº(id), capacidad, precio por día, estado (DISPONIBLE, BRUTA, OCUPADA)
+ 
+# Reserva -> nom, cognom, dni, telefon
+
+# La aplicación tiene que ser persistente, los datos tienen que quedar guardados en ficheros
+# de manera que cuando se cierre, la aplicación no pierda los datos registrados
+# y se puedan cargar a la siguiente ejecución. Los datos se tienen que guardar
+# en una carpeta llamada dades, dentro tienen que haber dos archivos
+# uno con los datos de habitaciones y otro con reservas.
+# se tiene que guardar con el mismo formato los dos archivos .txt.
+
+# Para comodidad, no existirá menú y la aplicació funcionará por línea de comandos.
+# ejemplo: py ./main.py afegir habitacio 100 1 45.90
+# en el archivo de habitacio tendria que quedar: 100, 1, 45.90, DISPONIBLE
+
+
+
+
---
+++ Python/UF3/practica fitxers/main.py
@@ -0,0 +1,6 @@
+import sys
+
+command = sys.argv
+if len(command) > 1:
+    if len(sys.argv[1]) == 2:
+
